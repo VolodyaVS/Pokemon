d4fa981e733000e72fe0b67b9b6bf891021e48ed
Vladimir Stepanchikov <volodyavolga@protonmail.ch>
2021-11-12 10:58:55 +0300

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Vladimir Stepanchikov on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
